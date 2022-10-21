3df5a023f102b9390e7221ab7c0e746a64e2cb06
gzh <2498495444@qq.com>
2022-10-22 01:29:40 +0800

    second change

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestGitApplication {
     //first change
+    //second change
     public static void main(String[] args) {
         SpringApplication.run(TestGitApplication.class, args);
     }
