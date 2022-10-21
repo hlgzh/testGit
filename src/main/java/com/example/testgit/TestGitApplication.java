bff6cc3559544bf64f17ef56bf6f5734bfc06027
gzh <2498495444@qq.com>
2022-10-22 01:32:07 +0800

    new branch first

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestGitApplication {
-    //first change
-    //second change
+    //new branch first
     public static void main(String[] args) {
         SpringApplication.run(TestGitApplication.class, args);
     }
